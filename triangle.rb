2b2dd3b64da901f4315eb9cbd0f822bf545a4ee9
whw-baird <wbaird@chicagobooth.edu>
2022-04-05 00:33:11 +0000

    finished about_triangle_project

--- triangle.rb
+++ triangle.rb
@@ -15,6 +15,13 @@
 #
 def triangle(a, b, c)
   # WRITE THIS CODE
+  if a == b && b == c
+      :equilateral
+  elsif a == b || a == c || b == c
+      :isosceles
+  else
+      :scalene   
+ end
 end
 
 # Error class used in part 2.  No need to change this code.
