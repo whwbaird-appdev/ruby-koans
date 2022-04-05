3396c5c6ce3f47703a7ed7ced5c9e0d2610fded5
whw-baird <wbaird@chicagobooth.edu>
2022-04-05 03:28:38 +0000

    finished about_dice_project

--- about_inheritance.rb
+++ about_inheritance.rb
@@ -24,21 +24,21 @@ class AboutInheritance < Neo::Koan
   end
 
   def test_subclasses_have_the_parent_as_an_ancestor
-    assert_equal __, Chihuahua.ancestors.include?(Dog)
+    assert_equal true, Chihuahua.ancestors.include?(Dog)
   end
 
   def test_all_classes_ultimately_inherit_from_object
-    assert_equal __, Chihuahua.ancestors.include?(Object)
+    assert_equal true, Chihuahua.ancestors.include?(Object)
   end
 
   def test_subclasses_inherit_behavior_from_parent_class
     chico = Chihuahua.new("Chico")
-    assert_equal __, chico.name
+    assert_equal "Chico", chico.name
   end
 
   def test_subclasses_add_new_behavior
     chico = Chihuahua.new("Chico")
-    assert_equal __, chico.wag
+    assert_equal :happy, chico.wag
 
     assert_raise(___) do
       fido = Dog.new("Fido")
