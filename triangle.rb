205de759136d100ae9cd286932e95fad78d742e8
whw-baird <wbaird@chicagobooth.edu>
2022-04-05 00:56:15 +0000

    finished about_triange_project_2

--- triangle.rb
+++ triangle.rb
@@ -15,6 +15,12 @@
 #
 def triangle(a, b, c)
   # WRITE THIS CODE
+  if a + b <= c || a + c <= b || b + c <= a
+    raise TriangleError
+  elsif
+    a <= 0 || b <= 0 || c <= 0
+    raise TriangleError
+  end
   if a == b && b == c
       :equilateral
   elsif a == b || a == c || b == c
@@ -22,6 +28,7 @@ def triangle(a, b, c)
   else
       :scalene   
  end
+
 end
 
 # Error class used in part 2.  No need to change this code.
