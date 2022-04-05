970fee3aa620ba4a10cf79a221214e1712b77fe9
whw-baird <wbaird@chicagobooth.edu>
2022-04-05 00:33:48 +0000

    finished about_triangle_project

--- about_exceptions.rb
+++ about_exceptions.rb
@@ -6,10 +6,10 @@ class AboutExceptions < Neo::Koan
   end
 
   def test_exceptions_inherit_from_Exception
-    assert_equal __, MySpecialError.ancestors[1]
-    assert_equal __, MySpecialError.ancestors[2]
-    assert_equal __, MySpecialError.ancestors[3]
-    assert_equal __, MySpecialError.ancestors[4]
+    assert_equal RuntimeError, MySpecialError.ancestors[1]
+    assert_equal RuntimeError, MySpecialError.ancestors[2]
+    assert_equal RuntimeError, MySpecialError.ancestors[3]
+    assert_equal RuntimeError, MySpecialError.ancestors[4]
   end
 
   def test_rescue_clause
